9cb03d71afe9e77f34fda636be1a911dc98feb73
Administrator <Administrator@PC195>
2020-06-05 14:58:36 +0700

    Create

---
+++ BigSchool/ViewModels/CourseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseModel
+    {
+        public string Place { get; set; }
+        public string Time { get; set; }
+        public string Date { get; set; }
+    }
+}
